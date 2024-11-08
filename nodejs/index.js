23e16338172811f2e907741d60c47cb17445e9dc
iaalvearp <iaalvearp@gmail.com>
2024-11-08 12:30:13 -0500

    Learning nodeJS con fazt

---
+++ nodejs/index.js
@@ -0,0 +1,4 @@
+const showUserInfo = (userName, userAge) => `The username is ${userName}, the user is ${userAge} years old.`
+
+console.log(showUserInfo('Joe', 30))
+console.log(showUserInfo('Maria', 50))
